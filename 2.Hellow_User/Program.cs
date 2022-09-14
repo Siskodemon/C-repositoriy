1ef104e482ad7152f0887ae5df6ce33b18b53d2b
Siskodemon <whiteprist@gmail.com>
2022-09-14 21:38:28 +0900

    2 программа(Hellow_User) написана

--- 2.Hellow_User/Program.cs
+++ 2.Hellow_User/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Нашего друга зовут ");
+string name = Console.ReadLine();
+Console.Write("Привет, ");
+Console.WriteLine(name);
