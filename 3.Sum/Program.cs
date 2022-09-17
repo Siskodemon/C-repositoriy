98453a2dd7510d886cdc23261af3def49ab6ffea
Siskodemon <whiteprist@gmail.com>
2022-09-17 15:20:57 +0900

    Добавлено деление

---
+++ 3.Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите первое слагаемое: ");
+string a = Console.ReadLine();
+Console.Write("Введите второе слагаемое: ");
+string b = Console.ReadLine();
+Console.Write("Сумма этих двух чисел равна - ");
+Console.WriteLine(Convert.ToInt64(a)+Convert.ToInt64(b));
---
+++ 4.Delenie/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите делимое: ");
+string a = Console.ReadLine();
+Console.Write("Введите делитель: ");
+string b = Console.ReadLine();
+Console.Write("Результат деления равен - ");
+Console.WriteLine(Convert.ToDouble(a)/Convert.ToDouble(b));
