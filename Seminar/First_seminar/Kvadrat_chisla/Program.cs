c366083c12ea7681a7314b22da917e12a1e0950a
Siskodemon <whiteprist@gmail.com>
2022-09-17 16:38:24 +0900

    Добавлена задача семинара и начато решение задачи с лекции про гири

---
+++ Seminar/First_seminar/Kvadrat_chisla/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Квадрат числа равен: "+ chislo*chislo);
+
