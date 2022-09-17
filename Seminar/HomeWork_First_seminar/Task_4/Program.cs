77f151750d067e3bc9bb81ebca71ca7bf68e4ba5
Siskodemon <whiteprist@gmail.com>
2022-09-17 20:28:14 +0900

    Добавлено четвёртое домашнее задание по семинару №1

--- Seminar/HomeWork_First_seminar/Task_4/Program.cs
+++ Seminar/HomeWork_First_seminar/Task_4/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Последовательность чисел: ");
+if(a%2==0)
+    for(int i=2;i<=a;i=i+2)
+    Console.Write(i+" ");
+     else{
+        for(int i=2;i<a;i=i+2)
+        Console.Write(i+" ");
+     }
