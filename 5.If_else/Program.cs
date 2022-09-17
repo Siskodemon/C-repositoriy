e4068bb38050c628336dff24294b4132df706288
Siskodemon <whiteprist@gmail.com>
2022-09-17 15:56:23 +0900

    Добавлен оператор If-Else

---
+++ 5.If_else/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Write("Введите имя пользователя: ");
+string name = Console.ReadLine();
+if(name == "Маша"){
+    Console.WriteLine("Приветствуем несравненную Машу");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(name);
+}
